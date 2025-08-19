60687bfc358035d12f3ff7df1db036d7495ce357
LUCAS SECO ALVES <lucas.seco@cs.up.edu.br>
2025-08-18 21:45:02 -0300

    Aula sobre APIs

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Funcionalidades - Requisições
+// - URL/Caminho/Endereço
+// - Um metodo HTTP
+
+
+app.MapGet("/", () => "Minha segunda API em C#");
+
+app.MapGet("/endereco", () => "Outra funcionalidade!");
+
+app.MapPost("/endereco", () => "Funcionalidade!");
+
+
+
 
 app.Run();
